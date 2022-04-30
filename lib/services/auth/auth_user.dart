c021369a7aeb379cbb4e019ccd763f4734f6f528
autobots33 <sushantt702@gmail.com>
2022-04-30 14:29:05 +0545

    eighth commit

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -5,6 +5,9 @@ import 'package:flutter/foundation.dart';
 @immutable //  Annotation telling that this class or any subclasses  of this class are going to be immutable meaning that their internals are never going to be changed upon initialization
 class AuthUser{
   final bool isEmailVerified;
-  const AuthUser(this.isEmailVerified);
-  factory AuthUser.fromFirebase(User user)=>AuthUser(user.emailVerified);
+  const AuthUser({required this.isEmailVerified});
+  factory AuthUser.fromFirebase(User user)=>
+      AuthUser(isEmailVerified:user.emailVerified);
+
+
 }
